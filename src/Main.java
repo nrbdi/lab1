e28e206ba1497b699d695505802f4893c0fd4377
nrbdi <242250@astanait.edu.kz>
2025-03-30 15:54:36 +0500

    task4

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,19 @@
 import java.util.Scanner;
-//task3
-//The function recursively checks divisibility up to √n, making it O(√n)
+//task4
+//The function makes n recursive calls, which makes it's time complexity O(n)
+
 
 public class Main {
-    public static boolean isPrime(int n, int i) {
-        if (n<2) return false;
-        if (i*i>n) return true;
-        if (n % i == 0) return false;
-        return isPrime(n, i+1);
+    public static int factorial(int n) {
+        if (n == 0 || n == 1) return 1;
+        return n * factorial(n - 1);
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
 
-        if (isPrime(n, 2))
-            System.out.println("Prime");
-        else
-            System.out.println("Composite");
-
+        System.out.println(factorial(n));
         scanner.close();
     }
 }
