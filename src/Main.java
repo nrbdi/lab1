f0015d6ed9c06267f762199f7a2517c7eb4b4b0e
nrbdi <242250@astanait.edu.kz>
2025-03-30 16:29:53 +0500

    task7

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,20 @@
 import java.util.Scanner;
-//task6
-//The function makes n recursive calls, reducing n by 1 each time, so it is O(n)
+//task7
+//in the function each element is read once and printed once, so it is O(n)
 
 public class Main {
-    public static int power(int a, int n) {
-        if (n == 0) return 1;
-        return a * power(a, n - 1);
+    public static void reversePrint(int index, int n, Scanner scanner) {
+        if (index == n) return;
+        int num = scanner.nextInt();
+        reversePrint(index + 1, n, scanner);
+        System.out.print(num + " ");
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
         int n = scanner.nextInt();
-
-        System.out.println(power(a, n));
+        reversePrint(0, n, scanner);
         scanner.close();
     }
 }
-
 
